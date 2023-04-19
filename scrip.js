9a5ae17acad22f4e8ace4fbd39b213eb9b919518
Gabriel <gabriel.soaressc58@gmail.com>
2023-04-19 14:26:14 +0000

    solve bug [duply pokemons]

--- scrip.js
+++ scrip.js
@@ -1,6 +1,6 @@
 
 let listElement = document.querySelector("ul");
-const inputPokemon = document.querySelector("#inputPokemon")
+const inputPokemon = document.querySelector("#inputPokemon");
 const URL_API = 'https://pokeapi.co/api/v2/pokemon';
 let URL_IMAGE = '';
 
@@ -15,35 +15,37 @@ function loadPokemon(data) {
 
     const { forms, sprites } = data;
 
-    console.log(data);
-    for (item of forms) {
-        const liElement = document.createElement("li");
-        const div = document.createElement("div");
-        const h2 = document.createElement("h2");
-        const span = document.createElement("span");
-        const textH2 = document.createTextNode(item.name.toUpperCase());
-        const textSpan = document.createTextNode("Biografia")
+    for (const index in forms) {
+        const item = forms[index];
 
-        const img = document.createElement("img");
-        img.setAttribute("src", sprites.other.dream_world.front_default);
-        h2.appendChild(textH2)
-        span.appendChild(textSpan)
-        liElement.appendChild(h2)
-        div.appendChild(img);
-        div.appendChild(span);
-        liElement.appendChild(div)
-        listElement.appendChild(liElement);
-
+        if (index == 0) {
+            const liElement = document.createElement("li");
+            const div = document.createElement("div");
+            const h2 = document.createElement("h2");
+            const span = document.createElement("span");
+            const textH2 = document.createTextNode(item.name.toUpperCase());
+            const textSpan = document.createTextNode("Biografia");
+            const img = document.createElement("img");
+            
+            img.setAttribute("src", sprites.other.dream_world.front_default);
+            h2.appendChild(textH2);
+            span.appendChild(textSpan);
+            liElement.appendChild(h2);
+            div.appendChild(img);
+            div.appendChild(span);
+            liElement.appendChild(div);
+            listElement.appendChild(liElement);
+        }
     }
 }
 
-function limparElemento(elemento) {
+function clearElement(elemento) {
     listElement.innerHTML = '';
 }
 
-function pesquisar(event) {
+function search(event) {
     event.preventDefault();
-    limparElemento(listElement)
-    const valorInput = inputPokemon.value;
-    find(valorInput)
+    clearElement(listElement);
+    const inputValue = inputPokemon.value.toLowerCase();
+    find(inputValue);
 }
