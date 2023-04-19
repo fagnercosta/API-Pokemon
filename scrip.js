a3d21a0afe0917f346011d90fea16f90f2a624bb
Gabriel <gabriel.soaressc58@gmail.com>
2023-04-19 13:28:51 +0000

    format documents

--- scrip.js
+++ scrip.js
@@ -1,25 +1,22 @@
 
-let  listElement = document.querySelector("ul");
+let listElement = document.querySelector("ul");
 const inputPokemon = document.querySelector("#inputPokemon")
 const URL_API = 'https://pokeapi.co/api/v2/pokemon';
-let URL_IMAGE ='';
+let URL_IMAGE = '';
 
-
-function find(id){
-        fetch(`${URL_API}/${id}`)
-                .then(response=>response.json())
-                .then((data)=> loadPokemon(data))
-                .catch((error)=>{console.log(error)});
+function find(id) {
+    fetch(`${URL_API}/${id}`)
+        .then(response => response.json())
+        .then((data) => loadPokemon(data))
+        .catch((error) => { console.log(error) });
 }
 
+function loadPokemon(data) {
 
-function loadPokemon(data){
-    
-    const {forms, sprites} = data;
+    const { forms, sprites } = data;
 
-    
     console.log(data);
-    for (item of forms){
+    for (item of forms) {
         const liElement = document.createElement("li");
         const div = document.createElement("div");
         const h2 = document.createElement("h2");
@@ -28,23 +25,23 @@ function loadPokemon(data){
         const textSpan = document.createTextNode("Biografia")
 
         const img = document.createElement("img");
-        img.setAttribute("src",sprites.other.dream_world.front_default);
+        img.setAttribute("src", sprites.other.dream_world.front_default);
         h2.appendChild(textH2)
         span.appendChild(textSpan)
         liElement.appendChild(h2)
         div.appendChild(img);
         div.appendChild(span);
-        liElement.appendChild(div) 
+        liElement.appendChild(div)
         listElement.appendChild(liElement);
-        
+
     }
 }
 
-function limparElemento(elemento){
-    listElement.innerHTML='';
+function limparElemento(elemento) {
+    listElement.innerHTML = '';
 }
 
-function pesquisar(event){
+function pesquisar(event) {
     event.preventDefault();
     limparElemento(listElement)
     const valorInput = inputPokemon.value;
